fd22661f9d62c30d4468da27697096ac6c59b353
pepekat1 <katya_maslennikova_04@mail.ru>
2024-03-27 14:36:08 +0300

    вторая часть

---
+++ ConsoleApplication27.03/ConsoleApplication27.03/ConsoleApplication27.03.cpp
@@ -0,0 +1,7 @@
+﻿Qt = at * Qp;
+nt = Qt / qt;
+Qf = (1 - at) * Qp;
+nf = Qf / qf;
+cout << "Кол-во пельменных автоматов" << ceil(np);
+cout << "Кол-во тестомесителей" << ceil(nt);
+cout << "Кол-во куттеров" << ceil(nf);
